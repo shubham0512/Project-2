e24ffe4d8e0119b364ca13e2aa5f89e9f24a7e97
shubham0512 <shubham.jain0512@gmail.com>
2017-05-17 01:00:23 +0530

    Add files via upload

---
+++ IsDistrictCompleteTest.java
@@ -0,0 +1,29 @@
+
+import java.io.IOException;
+
+import org.apache.pig.FilterFunc;
+import org.apache.pig.data.Tuple;
+import java.text.DecimalFormat;
+
+
+public class IsDistrictCompleteTest extends FilterFunc {
+
+
+    public Boolean exec(Tuple input) throws IOException {
+	DecimalFormat df = new DecimalFormat("#.000");        
+	// expect  string
+   	boolean isComplete=false;
+        double objective = Integer.parseInt(input.get(0).toString());
+	double performance = Integer.parseInt(input.get(1).toString()); 	
+	// double calc = (objective/performance) * 100;
+        if (objective !=0 && objective != ' ' && performance != ' '){
+	    // DecimalFormat df = new DecimalFormat("#.00000");
+	    // df.format(0.912385)
+	    double calc = Double.valueOf(df.format((performance/objective) * 100));
+          if (calc >= Double.valueOf(df.format(80))) {	
+	    	isComplete=true;
+	}
+        }
+        return isComplete;
+    }
+}
